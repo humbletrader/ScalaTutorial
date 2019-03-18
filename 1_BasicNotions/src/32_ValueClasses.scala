6982100373f3224f17c817d48d3679511054ceeb
dragosbadswizz <dragos.balan@adswizz.com>
2019-03-18 09:55:48 +0200

    improvements in value classes, arras

--- 1_BasicNotions/src/32_ValueClasses.scala
+++ 1_BasicNotions/src/32_ValueClasses.scala
@@ -1,17 +1,21 @@
 //value classes are not stored in the heap
-//they offer better performance as compared to the reference types
+//they offer better performance as compared to the reference types however there are
 
-//see: https://docs.scala-lang.org/overviews/core/value-classes.html
-//see the above link for limitations
+
+//everything about value classes can be found here :
+//https://docs.scala-lang.org/overviews/core/value-classes.html
 
 //a value class can wrap a value type
 class Currency(val amount: Float) extends AnyVal {
   def prettyPrint : String = s"$amount dollars"
 }
 
-//but it can also wrap a non value ( i.e a reference ) type
+//but it can also wrap a non value (i.e a reference) type
 class PhoneNumber(val s: String) extends AnyVal {
   def digits : String = s.replaceAll("""\D""", "")
 }
-
-
+//Because the JVM does not support value classes, Scala sometimes needs to actually instantiate a value class
+//A value class is actually instantiated when :
+// * a value class is treated as another type.
+// * a value class is assigned to an array.
+// * doing runtime type tests, such as pattern matching.
